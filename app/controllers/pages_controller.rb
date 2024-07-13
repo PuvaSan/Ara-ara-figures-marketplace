a8295fda7716930ebd2b64a28105c5c49ee5442b
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-13 10:14:11 +0900

    settings work with skip before method now

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
-    skip_before_action :authenticate_user!, only: :home
   end
 end
