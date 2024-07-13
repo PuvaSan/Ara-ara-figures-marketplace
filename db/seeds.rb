1b36e833daca26b1c6bfaa8998e729458b5af795
Javier Ruiz <ruizjavi070@gmail.com>
2024-07-13 16:45:53 +0900

    made 2 users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.destroy_all
+
+User.create!([{
+  username: "brianUchiha",
+  first_name: "Brian",
+  last_name: "Inphouva",
+  email: "brianisloco@hotmail.com",
+  password: "123456",
+  bio: "Hello, konichiwa. Lorem ipsum dolor sit amet, ad nam omnes mediocritatem concludaturque, in ignota electram persequeris eos. Eirmod consulatu qui ei, nam insolens conclusionemque in, sit ad malis delenit iudicabit. An alterum corpora.",
+  address: "Tokyo"
+},
+{
+  username: "AyyyaSayonara",
+  first_name: "Aya",
+  last_name: "Fujita",
+  email: "Aya0123@gmail.com",
+  password: "secret",
+  bio: "Hiyyah! Simul labores facilis ius ea. Te mel quem audiam labores, has cu debet homero vivendum. At nec admodum recteque, cum aperiam maluisset rationibus ne.",
+  address: "Gunma"
+}])
+
+puts "Created #{User.count} users"
