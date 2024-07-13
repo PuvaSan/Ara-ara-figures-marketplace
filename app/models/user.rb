24529ef4ef751118b06fb147264adefd985e2c92
ikumi ojiro <ikumidudecool@gmail.com>
2024-07-13 16:17:39 +0900

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :validatable
+  validates :username, :first_name, :lastname, :address, :bio, presense: true
+  validates :username, :first_name, :lastname, :bio, uniquness: true
 end
