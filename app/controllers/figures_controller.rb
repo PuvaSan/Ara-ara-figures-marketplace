885e9f22c8e99dbb2ec981f20f867e0903ed1814
ikumi ojiro <ikumidudecool@gmail.com>
2024-07-16 22:06:34 +0900

    added _animecategory and added new anime category page

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,19 +1,23 @@
 class FiguresController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
     @figures = Figure.all
+    @animes = Anime.all
   end
 
   def new
     @figure = Figure.new
   end
 
-  # def create
-  #   @figure = Figure.create(figure_params)
-  # end
+  def create
+     @figure = Figure.create(figure_params)
+   end
+
 
   private
 
-  # def figure_params
-  #   params.require(:figure).permit()
-  # end
+  def figure_params
+     params.require(:figure).permit()
+   end
 end
