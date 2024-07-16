cd5ba06167508452a290359c638c619fe82adecf
Ayano Fujita <cathy.ayano@gmail.com>
2024-07-16 22:39:52 +0900

    seller-can-list-new-figure

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,4 +1,6 @@
 class FiguresController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
   def index
     @figures = Figure.all
   end
@@ -7,13 +9,20 @@ class FiguresController < ApplicationController
     @figure = Figure.new
   end
 
-  # def create
-  #   @figure = Figure.create(figure_params)
-  # end
+  def create
+    @figure = Figure.create(figure_params)
+    @figure.user = current_user
+    if @figure.save
+      # redirect_to list_path(@list)
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
-  # def figure_params
-  #   params.require(:figure).permit()
-  # end
+  def figure_params
+    params.require(:figure).permit(:name, :description, :price, :anime_id, :pick_up, :delivery, :photos)
+  end
 end
