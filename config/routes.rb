fa8386064983efc6a025a38679e4849e770887a8
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-17 19:50:59 +0900

    made side menu within dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'orders/index'
   devise_for :users
   root to: "figures#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
