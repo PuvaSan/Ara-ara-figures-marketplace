c7cefdbb257d6e1fbe1b82177dfed1b2578652b9
Ayano Fujita <cathy.ayano@gmail.com>
2024-07-18 10:45:09 +0900

    form for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,25 @@ class OrdersController < ApplicationController
   def index
     @orders = Order.all
   end
+
+  def new
+    @order = Order.new
+    @figure = Figure.find(params[:figure_id])
+  end
+
+  def create
+    @order = Order.create(order_params)
+    @order.buyer = current_user
+    if @order.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:figure_id, :pick_up, :delivery)
+  end
 end
