548ab572e0ef49eb8cdc1b143f38b253609361ed
ikumi ojiro <ikumidudecool@gmail.com>
2024-07-18 19:42:27 +0900

    figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,25 +1,20 @@
 class FiguresController < ApplicationController
-<<<<<<< HEAD
   skip_before_action :authenticate_user!
-=======
   skip_before_action :authenticate_user!, only: :index
->>>>>>> a6d777bf72a6248268e09e309464ea5f32766da4
 
   def index
     @figures = Figure.all
     @animes = Anime.all
-  end
+  # Remove the extra 'end' keyword
 
   def new
     @figure = Figure.new
-  end
+  # Remove the extra 'end' keyword
 
   def create
-<<<<<<< HEAD
      @figure = Figure.create(figure_params)
    end
 
-=======
     @figure = Figure.create(figure_params)
     @figure.user = current_user
     if @figure.save
@@ -29,16 +24,12 @@ class FiguresController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
->>>>>>> a6d777bf72a6248268e09e309464ea5f32766da4
 
   private
 
   def figure_params
-<<<<<<< HEAD
      params.require(:figure).permit()
    end
-=======
     params.require(:figure).permit(:name, :description, :price, :anime_id, :pick_up, :delivery, photos: [])
   end
->>>>>>> a6d777bf72a6248268e09e309464ea5f32766da4
 end
