3dd58d4320477e460e2e6643c5c909e2d310c9a9
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-19 17:25:39 +0900

    missed a class add dnone

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -11,22 +11,24 @@ export default class extends Controller {
     this.reviewButtonTarget.classList.add("d-none");
   }
 
-  // this hides the order button and shows the product button when the product button is clicked
+  // hides other list except product list when product button is clicked
   product() {
     this.orderButtonTarget.classList.add("d-none");
     this.productButtonTarget.classList.remove("d-none");
+    this.reviewButtonTarget.classList.add("d-none");
   }
 
-  // this hides the product button and shows the order button when the order button is clicked
+  // hides other list except order list when order button is clicked
   order() {
-    this.productButtonTarget.classList.add("d-none");
     this.orderButtonTarget.classList.remove("d-none");
+    this.productButtonTarget.classList.add("d-none");
+    this.reviewButtonTarget.classList.add("d-none");
   }
 
-  // this hides the buttons when the review button is clicked
+  // this hides other list except review list when the review button is clicked
   review() {
-    this.productButtonTarget.classList.add("d-none");
     this.orderButtonTarget.classList.add("d-none");
+    this.productButtonTarget.classList.add("d-none");
     this.reviewButtonTarget.classList.remove("d-none");
   }
 }
