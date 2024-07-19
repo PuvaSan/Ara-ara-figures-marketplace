c169d442de82748d5b34f9d42d772410c5207183
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-19 16:53:27 +0900

    switch between orders and products on side panel menu now works with JS stimulus

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,14 +2,22 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["togglableElement"]
+  static targets = ["orderButton", "productButton"]
 
   connect() {
     console.log("Hello, Stimulus!", this.element);
+    this.productButtonTarget.classList.add("d-none");
   }
 
-  fire() {
-    console.log("Fire!", this.element);
-    this.togglableElementTarget.classList.toggle("d-none");
+  product() {
+    console.log("product!", this.element);
+    this.orderButtonTarget.classList.add("d-none");
+    this.productButtonTarget.classList.remove("d-none");
+  }
+
+  order() {
+    console.log("order!", this.element);
+    this.productButtonTarget.classList.add("d-none");
+    this.orderButtonTarget.classList.remove("d-none");
   }
 }
