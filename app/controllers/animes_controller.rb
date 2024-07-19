bc7ed1d992008eccb83f8f8d3fe1b2418d0e6159
Javier Ruiz <ruizjavi070@gmail.com>
2024-07-20 00:08:19 +0900

    figures can be listed by anime title

--- app/controllers/animes_controller.rb
+++ app/controllers/animes_controller.rb
@@ -5,5 +5,9 @@ class AnimesController < ApplicationController
 
   def show
     @anime = Anime.find(params[:id])
+    @figures = Figure.where(anime_id: @anime.id)
+    # @searched_anime = params[:anime] # Assume you get the anime title from params
+    # @figures = @figures.select { |figure, anime| anime == @searched_anime } if @searched_anime.present?
   end
+
 end
