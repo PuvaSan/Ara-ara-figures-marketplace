97c15e9b6ab13c7aeb8108eafcd3a67180908d69
Brian Inphouva <152071631+PuvaSan@users.noreply.github.com>
2024-07-20 13:44:14 +0900

    active menu works

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,9 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
-  static targets = ["orderButton", "productButton", "reviewButton"]
+  static targets = ["orderButton", "productButton", "reviewButton",
+                    "orderList", "productList", "reviewList"
+  ]
 
   // this hides product and order list when page loads
   connect() {
@@ -10,7 +12,7 @@ export default class extends Controller {
     this.productButtonTarget.classList.add("d-none");
     this.reviewButtonTarget.classList.add("d-none");
 
-    this.orderButtonTarget.classList.add("active");
+    this.orderListTarget.classList.add("active");
   }
 
   // hides other list except product list when product button is clicked
@@ -19,10 +21,12 @@ export default class extends Controller {
     this.productButtonTarget.classList.remove("d-none");
     this.reviewButtonTarget.classList.add("d-none");
 
-    this.productButtonTarget.classList.toggle("active");
-    this.orderButtonTarget.classList.remove("active");
-    this.reviewButtonTarget.classList.remove("active");
+    this.productListTarget.classList.add("active");
+    this.orderListTarget.classList.remove("active");
+    this.reviewListTarget.classList.remove("active");
   }
+
+
 
   // hides other list except order list when order button is clicked
   order() {
@@ -30,9 +34,9 @@ export default class extends Controller {
     this.productButtonTarget.classList.add("d-none");
     this.reviewButtonTarget.classList.add("d-none");
 
-    this.orderButtonTarget.classList.add("active");
-    this.productButtonTarget.classList.remove("active");
-    this.reviewButtonTarget.classList.remove("active");
+    this.orderListTarget.classList.add("active");
+    this.productListTarget.classList.remove("active");
+    this.reviewListTarget.classList.remove("active");
   }
 
   // this hides other list except review list when the review button is clicked
@@ -41,8 +45,8 @@ export default class extends Controller {
     this.productButtonTarget.classList.add("d-none");
     this.reviewButtonTarget.classList.remove("d-none");
 
-    this.reviewButtonTarget.classList.add("active");
-    this.productButtonTarget.classList.remove("active");
-    this.orderButtonTarget.classList.remove("active");
+    this.reviewListTarget.classList.add("active");
+    this.productListTarget.classList.remove("active");
+    this.orderListTarget.classList.remove("active");
   }
 }
